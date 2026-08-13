64ef8041159fa155d3ac543b3751622998213271
GUSTAVO QUEIROGA <gutomq@hotmail.com>
2026-08-13 14:25:25 -0300

    Atualiza e adiciona arquivo CSV da base varejo

--- MINI PROJETO AVALIATIVO/Projeto_analise_de_dados.py
+++ MINI PROJETO AVALIATIVO/Projeto_analise_de_dados.py
@@ -3,7 +3,8 @@
 # Autor: Gustavo Machado Queiroga
 # ============================================================
 
-import pandas as pd
+#Fiz um import para usar a biblioteca pandas, indicada para manipulação de dados
+import pandas as pd 
 
 # ============================================================
 # 1. CARREGAMENTO DA BASE
@@ -12,14 +13,15 @@ import pandas as pd
 # Caminho do arquivo CSV
 arquivo = "Base_Varejo.csv"
 
-# Carregando a base
+# Carregando a base e fazendo a leitura do arquivo CSV
 df = pd.read_csv(arquivo)
 
+# Aqui embaixo eu coloquei os símbolos de "=", pra criar um título visual mais bonito
 print("=" * 60)
 print("1. INFORMAÇÕES INICIAIS DA BASE")
 print("=" * 60)
 
-# Número de registros e colunas
+# Mostrando o número de registros e colunas
 print(f"\nNúmero de registros: {df.shape[0]}")
 print(f"Número de colunas: {df.shape[1]}")
 
